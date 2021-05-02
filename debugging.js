1f49397689093f8311fe4f8ad8278d6a5979d670
obrm <obrm770@gmail.com>
2021-05-02 19:59:06 +0300

    added comments

--- debugging.js
+++ debugging.js
@@ -15,38 +15,56 @@ Bonus:
   2. Add classes and ids to the widget (body.my-class > div#root > div.btn.btn-small)
 */
 
-const html = `
+// create a div with the class all-parents
+const markup = `
       <div class="all-parents" style="position: fixed; bottom: 0px; left: 0px; border: 5px dashed #fff; background-color: #000; color: #fff;width: 500px; height: 150px; padding: 50px; text-align: center; z-index: 1000;">
       </div>      
       `
 
-document.body.innerHTML += html
+// insert markup to body of the document
+document.body.innerHTML += markup
 
+// getParents function
 function getParents(el) {
+  // set parentSelector as document
   const parentSelector = document
 
+  // initialize parents array
   const parentsArray = []
 
+  // initialize parent variable as given el parentNode
   let parent = el.parentNode
 
+  // while the parent is not the parentSelector e.g. the document, and not null
   while (parent !== parentSelector && parent !== null) {
+    // set element variable equal the parent variable
     let element = parent
+    // unshift element.nodeName.toLowerCase to preventsArray
     parentsArray.unshift(element.nodeName.toLowerCase())
+    // set parent variable to be its own parent
     parent = element.parentNode
   }
 
+  // finally, unshift the document's nodeName.slice(1) itself to parentsArray
   parentsArray.unshift(parentSelector.nodeName.slice(1))
 
+  // initialize pTag variable to be an opening p tag
   let pTag = `<p style="font-size: 1rem">`
+  // set parents constant to be a string of parentsArray joined with ` > `
   const parents = parentsArray.join(` > `)
 
+  // concat pTag string with parents string and a closing p tag
   pTag += parents + `</p>`
 
+  // set innerHTML of div with "all-parents" class to be the pTag string
   document.querySelector('.all-parents').innerHTML = pTag
 }
 
+// set all as an HTMLCollection of all of the document's tags
 const all = document.getElementsByTagName('*')
 
+// loop through all elements in the document
 for (const el of all) {
+  // add to each element an event listener, so that on mouse over the getParents function will be called
   el.addEventListener('mouseover', (e) => getParents(e.target))
 }
--- findCarsByYear.js
+++ findCarsByYear.js
@@ -102,22 +102,35 @@ const INVENTORY = [
 ]
 
 function findCarsByYear(year) {
+  // create models array
   const models = []
+  // store Objects.values of INVENTORY to values constant
   const values = Object.values(INVENTORY)
+  // loop through values
   for (const item of values) {
+    // push in each iteration item's brand and the brand's models array
+    // of this brand to models array as an array of each of those pair,
+    // instead of an object, as in the values array
     models.push([item.brand, item.models])
   }
 
+  // initialize i as 1
   let i = 1
+  // initialize j as 0
   let j = 0
 
+  // loop through models array
   for (const item of models) {
+    // while j equals to 0 up to models.length - 1
     while (j <= models.length - 1) {
+      // check if there is an item[0][j] and if item[0][j].years includes given year
       if (item[i][j] && item[i][j].years.includes(year)) {
         console.log(`${item[0]} ${item[i][j].name} ${year}`)
       }
+      // increment j by 1
       j++
     }
+    // reset j to 0
     j = 0
   }
 }
@@ -125,9 +138,13 @@ function findCarsByYear(year) {
 findCarsByYear(2013)
 
 const findCarsByYear2 = (year) => {
+  // loop through INVENTORY
   INVENTORY.forEach((car) => {
+    // destructure brand out of each car element
     const { brand } = car
+    // loop through each car's models array
     car.models.forEach((model) => {
+      // check if models.years includes given year
       if (model.years.includes(year)) {
         console.log(`${brand} ${model.name} ${year}`)
       }
